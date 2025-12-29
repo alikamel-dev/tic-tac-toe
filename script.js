e8bc61e6754598658881b9dfff20ca6ebbde6cf3
Ali Kamel <ali.kamel.ali.2004@gmail.com>
2025-12-30 00:26:10 +0200

    Allow starting a new game by pressing Enter key

--- script.js
+++ script.js
@@ -649,6 +649,13 @@ const main = function () {
         // Custom confirmation dialogs (created using `window.confirm`) are ignored and may cause errors.
         window.addEventListener('beforeunload', event => event.preventDefault());
 
+        const submitEvent = new Event('submit', { bubbles: true, cancelable: true });
+
+        document.addEventListener('keydown', (event) => {
+            if (event.key === "Enter")
+                form.dispatchEvent(submitEvent);
+        });
+
         // Public variables
 
         function displayNewGameScreen() {
